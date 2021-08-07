5d7874799215aa2bda849268911a243ad9e50728
trigya7701 <rwikroy123@gmail.com>
2021-08-07 19:51:38 +0530

    Day 1 commit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,100 @@
 import Head from 'next/head'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer';
+import Header from '../components/Header'
+import LargeCard from '../components/LargeCard';
+import MediumCard from '../components/MediumCard';
+import SmallCard from '../components/SmallCard';
+import img1 from '../public/img1.webp';
+import img2 from '../public/img2.webp';
+import img3 from '../public/img3.webp';
+import img4 from '../public/img4.webp';
 
-export default function Home() {
+export default function Home({exploreData,cardsData}) {
+
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
-        <title>Create Next App</title>
+        <title>Airbnb Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* Header */}
+      <Header />
+      {/* Banner */}
+      <Banner />
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4" > 
+              {exploreData?.map(({img,location,distance})=>(
+                <SmallCard key={location} img={img} location={location} distance={distance}/>
+              ))}
+          </div>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+        </section>
+          
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
+        <LargeCard img={"https://links.papareact.com/4cj"}
+          title="The Greatest Outdoors"
+          description="Wishlists created by Airbnb"
+          buttonText="Get Inspired"
+          />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map(({img,title})=>(
+              <MediumCard key={img} img={img} title={title}/>
+            ))}
+          </div>
+         
+        </section>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <section >
+          <h2 className="text-4xl font-semibold py-8">Discover things to do</h2>
+          <div className="flex justify-between  space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            
+              <MediumCard  img={img2} title="Featured Collection"/>
+              <MediumCard  img={img3} title="Online Experiences"/>
+              <MediumCard  img={img4} title="Experiences"/>
+           
+          </div>
+         
+        </section>
+          <div className="text-white">
+            
+          <LargeCard img={img1}
+            title="Try Hosting"
+            description="Earn extra income and unlock new  oppurtunities by sharing your space."
+            // buttonText="Learn More"
+            />   
+          </div> 
+
+       
       </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
+      <hr className="bg-gray-300"/>
+      <Footer />
+      <div className="bg-gray-100 px-32 py-5">
+         <hr />
+          <h3>© 2021 Trigya Roy</h3>
+      </div>
+      
     </div>
   )
+}
+
+export async function getStaticProps(){
+  const exploreData=await fetch('https://jsonkeeper.com/b/ETBJ').then((res)=>res.json());
+  const cardsData=await fetch('https://links.papareact.com/zp1').then((res)=>res.json());
+  
+  return{
+    props:{
+      exploreData,
+      cardsData
+      
+    }
+  }
 }
