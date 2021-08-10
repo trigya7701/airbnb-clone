f5ae80bf05deaf95c94ef15e1100dfba05604f07
trigya7701 <rwikroy123@gmail.com>
2021-08-10 20:36:05 +0530

    Day 2 commit : Search Page /Calender

---
+++ pages/search.js
@@ -0,0 +1,68 @@
+import { format } from "date-fns";
+import { useRouter } from "next/dist/client/router"
+import Footer from "../components/Footer"
+import Header from "../components/Header"
+import InfoCarfd from "../components/InfoCarfd";
+
+const Search = ({searchResults}) => {
+    const router=useRouter();
+    const {location,startDate,endDate,noOfGuests}=router.query;
+    const formattedStartDate= format(new Date(startDate),"dd MMMM yy");
+    const formattedEndtDate= format(new Date(endDate),"dd MMMM yy");
+    const range=`${formattedStartDate}-${formattedEndtDate}`;
+    return (
+        <div>
+            <Header placeholder={`${location} | ${range} | ${noOfGuests}`} />
+            <main className="flex">
+                <section className="flex-grow pt-14 px-6">
+
+                
+                    <p className="text-xs ">300 + stays {range} for {noOfGuests} Guests</p>
+                    <h1 className="text-3xl font-semibold  mt-2 mb-6">Stays in {location}</h1>
+                    <div className=" hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap">
+                        <p className="button">Cancellation Flexibility</p>
+                        <p className="button">Type of Place</p>
+                        <p className="button">Price</p>
+                        <p className="button">Rooms and Beds</p>
+                        <p className="button">More Filters</p>
+                        
+                    </div>
+                    <div className="flex flex-col">
+                    {searchResults.map(({img,location,title,description,star,price,
+                            total})=>(
+                        <InfoCarfd 
+                        key={location}
+                        img={img}
+                        location={location}
+                        title={title}
+                        description={description}
+                        star={star}
+                        price={price}
+                        total={total}
+
+                        />
+                    ))}
+                    </div>
+                   
+                </section>
+
+            </main>
+            <Footer />
+            <div className="bg-gray-100 px-32 py-5">
+                    <hr />
+                    <h3>© 2021 Trigya Roy</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Search
+export async function getServerSideProps(){
+    const  searchResults= await  fetch("https://links.papareact.com/isz").then((res)=>res.json());
+
+    return{
+        props:{
+            searchResults
+        }
+    }
+}
